74d94057d86a6b7b0cd3c894f6d8f4377b23a0f2
Stephanie Federman <sfederma@yahoo.com>
2021-09-22 17:51:23 -0700

    update front routes

--- pixeloos/src/api.js
+++ pixeloos/src/api.js
@@ -2,20 +2,20 @@ import axios from 'axios';
 
 require('dotenv').config();
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+// const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
 const DEV_APPROVAL_KEY = process.env.REACT_APP_DEV_APPROVAL_KEY || 'thissupersecretkeyfornow';
 
 class AnimalsApi {
     static token;
     static async login(data){
-        let res = await axios.post(`${BASE_URL}/users/login`,data)
+        let res = await axios.post(`/users/login`,data)
         if (res.data.error){
             throw res.data.error.msg
         }
         return res.data.user
     }
     static async register(data){
-        let res = await axios.post(`${BASE_URL}/users/new`,data)
+        let res = await axios.post(`/users/new`,data)
         if (res.data.error){
             throw res.data.error.msg
         }
@@ -23,7 +23,7 @@ class AnimalsApi {
     }
     static async getUser(usr_id) {
         try {
-            let res = await axios.get(`${BASE_URL}/users/${usr_id}`,{
+            let res = await axios.get(`/users/${usr_id}`,{
                 headers: {
                     Authorization: `Bearer ${AnimalsApi.token}`
                 }
@@ -44,7 +44,7 @@ class AnimalsApi {
     }
     static async addGold(usr_id, gold){
         let data = {usr_id:usr_id, goldToAdd: gold, devKey: DEV_APPROVAL_KEY}
-        let res = await axios.put(`${BASE_URL}/gold/add`,data,{
+        let res = await axios.put(`/gold/add`,data,{
             headers: {
                 Authorization: `Bearer ${AnimalsApi.token}`
             }
@@ -55,7 +55,7 @@ class AnimalsApi {
         return res.data.updatedUser;
     }
     static async newPet(data,usr_id){
-        let res = await axios.post(`${BASE_URL}/pets/new/${usr_id}`,data,{
+        let res = await axios.post(`/pets/new/${usr_id}`,data,{
             headers: {
                 Authorization: `Bearer ${AnimalsApi.token}`
             }
@@ -67,12 +67,12 @@ class AnimalsApi {
     }
     static async buyItem(data, usr_id, goldDec){
         data.devKey = DEV_APPROVAL_KEY;
-        let res = await axios.post(`${BASE_URL}/inventories/add`,data,{
+        let res = await axios.post(`/inventories/add`,data,{
             headers: {
                 Authorization: `Bearer ${AnimalsApi.token}`
             }
         })
-        let res2 = await axios.put(`${BASE_URL}/gold/dec`,{usr_id:usr_id, goldToDec: goldDec},{
+        let res2 = await axios.put(`/gold/dec`,{usr_id:usr_id, goldToDec: goldDec},{
             headers: {
                 Authorization: `Bearer ${AnimalsApi.token}`
             }
@@ -86,7 +86,7 @@ class AnimalsApi {
         return res.data.newInv;
     }
     static async feedPet(data, usr_id, pet_id){
-        let res = await axios.put(`${BASE_URL}/pets/${usr_id}/${pet_id}/feed`,data,{
+        let res = await axios.put(`/pets/${usr_id}/${pet_id}/feed`,data,{
             headers: {
                 Authorization: `Bearer ${AnimalsApi.token}`
             }
@@ -97,7 +97,7 @@ class AnimalsApi {
         return res.data.pet;
     }
     static async playPet(data, usr_id, pet_id){
-        let res = await axios.put(`${BASE_URL}/pets/${usr_id}/${pet_id}/play`,data,{
+        let res = await axios.put(`/pets/${usr_id}/${pet_id}/play`,data,{
             headers: {
                 Authorization: `Bearer ${AnimalsApi.token}`
             }
@@ -111,7 +111,7 @@ class AnimalsApi {
         const headers = {
             Authorization: `Bearer ${AnimalsApi.token}`
           }
-        let res = await axios.delete(`${BASE_URL}/inventories/remove`, {headers,data});
+        let res = await axios.delete(`/inventories/remove`, {headers,data});
         if (res.data.error){
             throw res.data.error.msg
         }
@@ -120,7 +120,7 @@ class AnimalsApi {
 
     static async addExp(data, pet_id, usr_id){
         data.devKey = DEV_APPROVAL_KEY;
-        let res = await axios.put(`${BASE_URL}/pets/${usr_id}/${pet_id}/exp`,data,{
+        let res = await axios.put(`/pets/${usr_id}/${pet_id}/exp`,data,{
             headers: {
                 Authorization: `Bearer ${AnimalsApi.token}`
             }
